8719b180cdfc007417cd9c0657c356aad928740b
Hélio Rocha <hjrocha@gmail.com>
2019-10-11 17:50:29 +0100

    express app

--- src/server/index.js
+++ src/server/index.js
@@ -1,2 +1,10 @@
 const express = require('express')
 
+const app = express()
+
+app.use(bodyParser.urlencoded({
+    extended: true
+ }));
+ app.set('json spaces', 2)
+
+ app.use(bodyParser.json());
