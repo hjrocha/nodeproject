eb8758ccf4b17c24b4d5a7bc05ebe81fce321305
Hélio Rocha <hjrocha@gmail.com>
2019-10-11 17:53:29 +0100

    some issues

--- src/server/lib/people.js
+++ src/server/lib/people.js
@@ -2,5 +2,23 @@
 var fs = require("fs");
 
 exports.doSomething = function() {
-    // ...
+    for (var i = 0; i < 10; i--) {
+    }
+    
+    for (var i = 10; i >= 0; i++) {
+    }
+
+    var x = 5;;
+
+    var key = 'key'
+
+    var object = {
+        key: 'yes!'
+    }
+
+    if (!key in object) {
+        // operator precedence makes it equivalent to (!key) in object
+        // and type conversion makes it equivalent to (key ? "false" : "true") in object
+    }
+
 };
