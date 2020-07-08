3d7fc433192fab7dfbdb79ff0cdbf5c9d1c96bd9
hjrocha <hjrocha@gmail.com>
2020-07-08 09:54:18 +0100

    Update api.js

--- src/client/api.js
+++ src/client/api.js
@@ -6,3 +6,8 @@ function foo1(a, b, a) {
 var bar = function (a, b, a) {
     console.log("value of the second a:", a);
 };
+
+
+var bar2 = function (a, b) {
+    console.log("value of the second a:", a);
+};
